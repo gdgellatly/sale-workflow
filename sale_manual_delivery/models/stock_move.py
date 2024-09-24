0da7186d7d500b193e414e6ebe791e59424efbf4
Graeme Gellatly <graeme@o4sb.com>
2024-09-24 21:38:25 +1200

    Functional Core 2

--- sale_manual_delivery/models/stock_move.py
+++ sale_manual_delivery/models/stock_move.py
@@ -9,47 +9,21 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     def _get_new_picking_values(self):
-        # Overload to set carrier_id from the manual delivery wizard
-        # Note: sale_manual_delivery is expected to be a manual.delivery record
+        """Overload to set partner_id and carrier_id from
+        the manual delivery wizard
+        """
         res = super()._get_new_picking_values()
-        manual_delivery = self.env.context.get("sale_manual_delivery")
-        if manual_delivery:
-            if manual_delivery.partner_id:
-                res["partner_id"] = manual_delivery.partner_id.id
-            if manual_delivery.carrier_id:
-                res["carrier_id"] = manual_delivery.carrier_id.id
+        if partner_id := self.env.context.get("manual_partner_id"):
+            res["partner_id"] = partner_id
+        if carrier_id := self.env.context.get("manual_carrier_id"):
+            res["carrier_id"] = carrier_id
         return res
 
-    def _search_picking_for_assignation(self):
-        # Overload to filter carrier_id
-        # Note: sale_manual_delivery is expected to be a manual.delivery record
-        manual_delivery = self.env.context.get("sale_manual_delivery")
-        if manual_delivery:
-            # original domain used in super()
-            domain = [
-                ("group_id", "=", self.group_id.id),
-                ("location_id", "=", self.location_id.id),
-                ("location_dest_id", "=", self.location_dest_id.id),
-                ("picking_type_id", "=", self.picking_type_id.id),
-                ("printed", "=", False),
-                ("immediate_transfer", "=", False),
-                (
-                    "state",
-                    "in",
-                    [
-                        "draft",
-                        "confirmed",
-                        "waiting",
-                        "partially_available",
-                        "assigned",
-                    ],
-                ),
-            ]
-            # Filter on carrier
-            if manual_delivery.carrier_id:
-                domain += [
-                    ("carrier_id", "=", manual_delivery.carrier_id.id),
-                ]
-            return self.env["stock.picking"].search(domain, limit=1)
-        else:
-            return super()._search_picking_for_assignation()
+    def _search_picking_for_assignation_domain(self):
+        """Overload to filter partner_id and carrier_id"""
+        domain = super()._search_picking_for_assignation_domain()
+        if partner_id := self.env.context.get("manual_partner_id"):
+            domain += [("partner_id", "=", partner_id)]
+        if carrier_id := self.env.context.get("manual_carrier_id"):
+            domain += [("carrier_id", "=", carrier_id)]
+        return domain
